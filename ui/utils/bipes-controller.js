6f520f0924d4a7dc3bffa5b51aaa1b5bbe39b95a
maxuecan <623875282@qq.com>
2026-05-26 17:08:49 +0800

    修改智能体

--- ui/utils/bipes-controller.js
+++ ui/utils/bipes-controller.js
@@ -11,6 +11,8 @@ import ExamplePreview from '@/components/example-preview.js'
 import SkulptController from './skulpt-controller.js'
 import EventEmitterController from './event-emitter-controller.js'
 
+import { initCoze } from './utils.js'
+
 export default class BipesController extends Common {
     constructor() {
         super()
@@ -26,7 +28,7 @@ export default class BipesController extends Common {
         this.SEARCH_DEVICE = null
         this.EXTENSIONS_DIALOG = null
 
-        this.COZE = this.initCoze()
+        this.COZE = initCoze()
 
         this._settings = this.getLocalSettings()
 
@@ -121,68 +123,5 @@ export default class BipesController extends Common {
         if (!this.EXTENSIONS_DIALOG) this.EXTENSIONS_DIALOG = new ExtensionsDialog()
         
         this.EXTENSIONS_DIALOG.show()
-    }
-
-    // 初始化智能体
-    initCoze() {
-        return new CozeWebSDK.WebChatClient({
-            config: {
-                // 智能体 ID
-                botId: '7504853922646360103'
-            },
-            auth: {
-                // 鉴权方式，默认type 为unauth，表示不鉴权；建议设置为 token，表示通过PAT或OAuth鉴权
-                type: 'token',
-                // type 为 token 时，需要设置PAT或OAuth访问密钥
-                token: 'pat_2qNEOaWYB7H2zt5aPahFF01OWlzq0QI1dYUI5V6bBdj7mdrKugweZMLp7UfXBUNy',
-                // 访问密钥过期时，使用的新密钥，可以按需设置。
-                onRefreshToken: async () => 'pat_2qNEOaWYB7H2zt5aPahFF01OWlzq0QI1dYUI5V6bBdj7mdrKugweZMLp7UfXBUNy'
-            },
-            // 用户信息
-            // userInfo: {
-            //     id: '',
-            //     url: 'https://lf-coze-web-cdn.coze.cn/obj/coze-web-cn/obric/coze/favicon.1970.png',
-            //     nickname: ''
-            // },
-            ui: {
-                // base: {
-                //   icon: '', // 应用图标，默认扣子图标
-                //   layout: '', // mobile pc，默认自动识别
-                //   lang: '', // 语言en zh-CN，默认英语
-                //   zIndex: 9999, // 层级
-                // },
-                // header: {
-                //   isShow: true, // 是否展示顶部标题栏
-                //   isNeedClose: true, // 是否展示右上角的关闭页面按钮
-                // },
-                // asstBtn: {
-                //   isNeed: true, // 浮动球，true 显示， false 不显示（显示聊天框：cozeWebSDK.showChatBot()，隐藏聊天框：cozeWebSDK.hideChatBot()）
-                // },
-                // footer: {
-                //   isShow: true, // 是否展示底部文案模块。
-                //   expressionText: 'Powered by {{name}}&{{name1}}', // 底部显示的文案信息
-                //   linkvars: {
-                //     name: {
-                //       text: 'A',
-                //       link: 'https://www.test1.com'
-                //     },
-                //     name1: {
-                //       text: 'B',
-                //       link: 'https://www.test2.com'
-                //     }
-                //   }, // 底部文案中的链接文案与链接地址
-                // },
-                chatBot: {
-                    title: '智能客服', // 聊天框的标题
-                    uploadable: true, // 是否开启聊天框的上传能力。
-                    // width: '', // PC 端窗口的宽度，单位为 px，默认为 460。
-                    // el: '', // 设置聊天框放置位置的容器（Element）
-                    // onHide: () => {}, // 当聊天框隐藏的时候，会回调该方法
-                    // onShow: () => {}, // 当聊天框显示的时候，会回调该方法
-                    // onBeforeShow: () => {}, //  聊天框显示前调用
-                    // onBeforeHide: () => {}, //  聊天框隐藏前调用
-                },
-            },
-        })
     }
 }
